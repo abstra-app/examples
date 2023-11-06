084b7f9c087ea8ba2e3230397dd8983431e75ac5
saboyaa <gabrielsaboya@live.com>
2023-11-06 20:30:00 -0300

    editing buying_intention

--- forms/buying_intention_form.py
+++ forms/buying_intention_form.py
@@ -6,10 +6,12 @@ This is a good example on how using Python scripts can be much simpler than form
 from abstra.forms import *
 from requests import post
 import os
+from dotenv import load_dotenv
+from slack_sdk import WebClient
 
-# This form uses an environment variable. To make it work properly, add a Slack API Key to your workspace's environment variables in the sidebar.
+# This form uses an environment variable. To make it work properly, add a Slack API Key to your wPorkspace's environment variables in the sidebar.
 token = os.environ.get("SLACK_BOT_TOKEN")
-
+client = WebClient(token=token)
 if "plan" in url_params:
     plan = url_params["plan"]
 else:
@@ -28,16 +30,18 @@ company = read("Company name")
 This is the quickest way you can avoid sending messages
 when someone on your company tests your script.
 """
-# if '@abstra.app' not in email:
-#     res = post(
-#         'https://slack.com/api/chat.postMessage',
-#         json={
-#             'channel': 'sales',
-#             'text': name + ' (' + email + ') wants to buy the ' + plan + ' plan'
-#         },
-#         headers={
-#             'Authorization': 'Bearer ' + token,
-#             'Content-type': 'application/json; charset=utf-8'
-#         })
+if '@abstra.app' not in email:
+    res = client.chat_postMessage(
+        channel="sa_planos",
+        text="Someone is interested in the "
+        + plan
+        + " plan. Their name is "
+        + name
+        + " and their email is "
+        + email
+        + ". Their company name is "
+        + company
+        + ".",
+    )
 
 display("We've got your information, we'll get in contact soon! 😉")
